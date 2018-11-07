495a53543747f2edeefa8c36244cecd3df3ac41a
Kaolin Fire <kaolin@erif.org>
2018-11-07 22:45:38 +0000

    various
      * minor cleanup

--- src/rdmac.c
+++ src/rdmac.c
@@ -11,6 +11,7 @@
 ////////////////////////////////////////////////////////////
 
 #include "reg.h"
+#include <stdio.h>
 
 int main(void) {
   if(spi_setup() != EXIT_SUCCESS){ // initialize wiringPi failed
@@ -21,6 +22,7 @@ int main(void) {
   uint buffer[6];
   spi_read_reg(0x0302, buffer, 6);
   spi_teardown();
+
   // write buffer contents to console
   printf("Switch MAC addr = ");
   for (uint index=0; index<6; index++) {
